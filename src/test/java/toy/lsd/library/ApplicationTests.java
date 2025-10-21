9172ce99ecaee109c92e50876f3d3cbb25d70d2c
lee-seongdong <lee-seongdong@nts-corp.com>
2025-10-21 18:54:31 +0900

    Study DDD

---
+++ src/test/java/toy/lsd/library/ApplicationTests.java
@@ -0,0 +1,12 @@
+package toy.lsd.library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests {
+	@Test
+	void test() {
+		assertEquals(2, 1 + 1);
+	}
+}
